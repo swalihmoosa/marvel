ddcc58c04fb2ed356ad5d6f0d001102eae786d89
swalihmoosa <swalihmoosa72@gmail.com>
2022-01-13 19:41:33 +0530

    React-integration | ongoing

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {Redirect, Route} from 'react-router-dom'
+import {Navigate, Route} from 'react-router-dom'
 
 
 export default function PrivateRoute({element:Element, ...rest}) {
+    const user_data = JSON.parse(localStorage.getItem("user_data"));
+
     return (
         <Route {...rest} render={(props)=>{
-            if (isLoggin){
+            if (user_data){
                 return <Element {...props} />
             }else{
-                return <Redirect to={{pathname:'/auth/login'}} />
+                return <Navigate to={{pathname:'/auth/login'}} />
             }
         }}
         />
