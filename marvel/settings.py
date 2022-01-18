20840c7247ca93693d114eafb216592e3bc2ec8c
swalihmoosa <swalihmoosa72@gmail.com>
2022-01-18 13:02:36 +0530

    API | Signup-ongoing

--- marvel/settings.py
+++ marvel/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'knox',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
--- web/api/views.py
+++ web/api/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics, permissions
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer
 
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -27,3 +30,19 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+
+
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
